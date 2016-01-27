a43b289047a7da9d4e20125eef60dd2cf0390650
chris <chris.the.marais@gmail.com>
2016-01-27 12:39:39 +0200

    Generalised things! Viterbi.py now contains a function which takes as input all the initial conditions to the algorithm. These conditions are now specified in run_initials.py and the Viterbi function is called. Dopalicious!

--- Viterbi.py
+++ Viterbi.py
@@ -1,67 +1,54 @@
 import numpy as np
 from functions import *
 
-states = ['hot', 'cold', 'small', 'medium', 'large' ]
-values= ['yes','maybe','no']
-#State Transition Probabilities [[h->h,c->h,s->h,m->h,l->h], [],[],[],[]]
-T = np.matrix([[0.6,0.8,0.,0.,0.],[0.4,0.2,0.,0.,0.],[0.,0.,0.,0.,0.],[0.,0.,1.,0.2,0.2],[0.,0.,0.,0.8,0.8]]) # T[i,j] means transfer TO i FROM j !!
-# State -> Observation "emission" probabilities  [[H->A,L->A],[H->C,L->C],[etc..],[]]
-E = np.matrix([[0.9,0.,0.3,0.4,0.8],[0.1,0.1,0.3,0.3,0.1],[0.,0.9,0.4,0.3,0.1]])
-# E[t,k] is the prob of emitting the t'th observation FROM the k'th state
-print("T = \n" + str(T))
-print("")
-print("E = \n" +str(E))
-#Data
-observations = ['no','no','no','no','no', 'no','no']
+def Viterbi(states, values, T, E, observations):
+    #Convert observations to numbers
+    y = []
+    for obs in observations:
+        for i in range(len(values)):
+            if obs == values[i]:
+                y.append(i)
 
 
-#Convert data
-y = []
-for value in observations:
-    if value == 'yes':
-        y.append(0)
-    elif value == 'maybe':
-        y.append(1)
-    elif value == 'no':
-        y.append(2)
-    else:
-        break
-        print("invalid measurement array. please leave!")
+    # Uniform 'beleif' distribution
+    p = []
+    for i in range(len(states)):
+        p.append(1./len(states))
 
-# Uniform 'beleif' distribution
-p = [0.2,0.2,0.2,0.2,0.2] #hot,cld,sml,med,lrg
+    #Initialize beleif dist (given first obs) and 'update-memory' for backtrack
+    # i.e. for t = 0
+    V = [[]]
+    B = [[]]
+    for i in range(len(states)):
+        V[0].append(p[i]*E[y[0],i]) #prior x emission prob
+        B[0].append(i)
+    V[0] = normalize(V[0])
+    print(V[0])
+    print(B[0])
+    #The real shit goes down
+    for t in range(1,len(observations)): #for each observed state (time step)
+        value = y[t]
+        V.append([])
+        B.append([])
+        for i in range(len(states)): #update each element in 'belief' distribution
+            transfer = []
+            for k in range(len(states)):
+                transfer.append( V[t-1][k]*E[value,k]*T[i,k] )
+            V[t].append(max(transfer))
+            B[t].append(np.argmax(transfer))
+        V[t] = normalize(V[t])
+        #print(V[t])
+        #print("This should equal 1: normalized sum = " + str(sum(V[t])) )
+        #print(B[t])
 
-#Initialize beleif dist (given first obs) and 'update-memory' for backtrack
-# i.e. for t = 0
-V = [[]]
-B = [[]]
-for i in range(len(states)):
-    V[0].append(p[i]*E[y[0],i]) #prior x emission prob
-    B[0].append(i)
-V[0] = normalize(V[0])
-print(V[0])
-print(B[0])
-#The real shit goes down
-for t in range(1,len(observations)): #for each observed state (time step)
-    value = y[t]
-    V.append([])
-    B.append([])
-    for i in range(len(states)): #update each element in 'belief' distribution
-        transfer = []
-        for k in range(len(states)):
-            transfer.append( V[t-1][k]*E[value,k]*T[i,k] )
-        V[t].append(max(transfer))
-        B[t].append(np.argmax(transfer))
-    V[t] = normalize(V[t])
-    print(V[t])
-    # print("normalized sum: " + str(sum(V[t])) )
-    print(B[t])
-# print(B)
-# print(V)
 
-#Backtrack to get most probable path
-index = np.argmax(V[len(observations)-1])
-bestpath = [index] # len(observations)-1 'th element
-for t in range(len(observations)-2 , -1,-1):
-    bestpath = [B[t+1][bestpath[0]]] + bestpath
-print("best path is " + str(bestpath))
+    #Backtrack to get most probable path
+    index = np.argmax(V[len(observations)-1])
+    bestpath = [index] # len(observations)-1 'th element
+    for t in range(len(observations)-2 , -1,-1):
+        bestpath = [B[t+1][bestpath[0]]] + bestpath
+    statepath = []
+    for j in bestpath:
+        statepath.append(states[j])
+
+    return V , statepath
---
+++ run_initials.py
@@ -0,0 +1,15 @@
+import Viterbi as vt
+import numpy as np
+states = ['hot', 'cold', 'small', 'medium', 'large' ]
+values= ['yes','maybe','no']
+#State Transition Probabilities [[h->h,c->h,s->h,m->h,l->h], [],[],[],[]]
+T = np.matrix([[0.6,0.8,0.,0.,0.],[0.4,0.2,0.,0.,0.],[0.,0.,0.,0.,0.],[0.,0.,1.,0.2,0.2],[0.,0.,0.,0.8,0.8]]) # T[i,j] means transfer TO i FROM j !!
+# State -> Observation "emission" probabilities  [[H->A,L->A],[H->C,L->C],[etc..],[]]
+E = np.matrix([[0.9,0.,0.3,0.4,0.8],[0.1,0.1,0.3,0.3,0.1],[0.,0.9,0.4,0.3,0.1]])
+# E[t,k] is the prob of emitting the t'th observation FROM the k'th state
+
+observations = ['no','no','no','no','no', 'no','no']
+
+
+path = vt.Viterbi(states,values,T,E,observations)[1]
+print(path)
